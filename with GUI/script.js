719fea0023a7068fcd731e77c3e3bbd86053a449
collinginsburg <chginsburg@email.wm.edu>
2021-04-29 17:12:36 -0700

    another huge dump. Added colors on hands to indicate a win or a lose. also went in a added the words win or lose next to the score. All it needs now is the rock paper scissor animation on each round...

--- with GUI/script.js
+++ with GUI/script.js
@@ -11,74 +11,104 @@
 //  set initial score
     let computerScore = 0;
     let playerScore = 0;
+    let roundWin;
 
     // plays a round, declares winner, adds score, and runs a score check
     function playRound(playerSelection, computerSelection){
 
         if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-            alert ("You win! Rock beats Scissors.");
+            // alert ("You win! Rock beats Scissors.");
+            roundWin = true;
             playerScore = playerScore + 1;
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
-            animations();
+            computerScoreboard.textContent = `Computer: ${computerScore} LOSE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} WIN!`;
+            displayPlayerRock();
+            displayComputerScissors();
+            playerRock.style.cssText = ('fill:green; display:block');
             checkScore();
- 
             return playerScore;
+
         }else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
             computerScore = computerScore + 1; 
-            alert ("You lose! Paper beats Rock");
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            // alert ("You lose! Paper beats Rock");
+            computerScoreboard.textContent = `Computer: ${computerScore} WIN!`;
+            playerScoreboard.textContent = `Player: ${playerScore} LOSE!`;
+            displayPlayerRock();
+            displayComputerPaper();
+            playerRock.style.cssText = ('fill:red; display:block');
             checkScore();
-
             return computerScore;
+
         }else if (playerSelection === "ROCK" && computerSelection === "ROCK"){
-            alert ("Tie!");
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            // alert ("Tie!");
+            computerScoreboard.textContent = `Computer: ${computerScore} TIE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} TIE!`;
+            displayPlayerRock();
+            displayComputerRock();
+            playerRock.style.cssText = ('fill:black; display:block');
             checkScore();
 
         }else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-            alert ("You lose. Rock beats Scissors");
+            // alert ("You lose. Rock beats Scissors");
             computerScore = computerScore + 1;
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            computerScoreboard.textContent = `Computer: ${computerScore} WIN!`;
+            playerScoreboard.textContent = `Player: ${playerScore} LOSE!`;
+            displayPlayerScissors();
+            displayComputerRock();
+            playerScissors.style.cssText = ('fill:red; display:block');
             checkScore();
-
             return computerScore;
-        }else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-            alert ("You win! Scissors beats Paper!");
-            playerScore = playerScore + 1;
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
-            checkScore();
 
+        }else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+            // alert ("You win! Scissors beats Paper!");
+            roundWin = true;
+            playerScore = playerScore + 1;
+            computerScoreboard.textContent = `Computer: ${computerScore} LOSE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} WIN!`;
+            checkScore();
+            displayPlayerScissors();
+            displayComputerPaper();
+            playerScissors.style.cssText = ('fill:green; display:block');
             return playerScore;
+
         }else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS"){
-            alert ("Tie!");
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            // alert ("Tie!");
+            computerScoreboard.textContent = `Computer: ${computerScore} TIE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} TIE!`;
+            displayPlayerScissors();
+            displayComputerScissors();
+            playerScissors.style.cssText = ('fill:black; display:block');
             checkScore();
 
         }else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
-            alert ("You win! Paper beats Rock!");
+            // alert ("You win! Paper beats Rock!");
+            roundWin = true;
             playerScore = playerScore + 1; 
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            computerScoreboard.textContent = `Computer: ${computerScore} LOSE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} WIN!`;
+            displayPlayerPaper();
+            displayComputerRock();
+            playerPaper.style.cssText = ('fill:green; display:block');
             checkScore();
 
             return playerScore;
         }else if (playerSelection === "PAPER" && computerSelection === "PAPER"){
-            alert ("Tie!");
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            // alert ("Tie!");
+            computerScoreboard.textContent = `Computer: ${computerScore} TIE!`;
+            playerScoreboard.textContent = `Player: ${playerScore} TIE!`;
+            displayPlayerPaper();
+            displayComputerPaper();
+            playerPaper.style.cssText = ('fill:black; display:block');
             checkScore();
 
         }else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
-            alert ("You lose! Scissors beats Paper.");
+            // alert ("You lose! Scissors beats Paper.");
             computerScore = computerScore + 1; 
-            computerScoreboard.textContent = `Computer: ${computerScore}`;
-            playerScoreboard.textContent = `Player: ${playerScore}`;
+            computerScoreboard.textContent = `Computer: ${computerScore} WIN!`;
+            playerScoreboard.textContent = `Player: ${playerScore} LOSE!`;
+            displayPlayerPaper();
+            displayComputerScissors();
+            playerPaper.style.cssText = ('fill:red; display:block');
             checkScore();
 
             return computerScore;
@@ -164,6 +194,12 @@ function resetGame(){
     playerScoreboard.textContent = `Player: ${playerScore}`;
     mainscreen.classList.remove('disappear');
     gameover.classList.add('disappear');
+    playerRock.style.cssText = "fill:black; display: none";
+    playerPaper.style.cssText = "fill:black; display: none";
+    playerScissors.style.cssText = "fill:black; display: none";
+    computerRock.style.cssText = "fill:black; display: none";
+    computerPaper.style.cssText = "fill:black; display: none";
+    computerScissors.style.cssText = "fill:black; display: none";
     console.clear();   
 }
 
@@ -173,4 +209,43 @@ function animations(){
 
 }
 
+
+// display hands...
+const playerRock = document.querySelector('.playerHand.rck');
+const playerPaper = document.querySelector('.playerHand.ppr');
+const playerScissors = document.querySelector('.playerHand.scs');
+const computerRock = document.querySelector('.computerHand.rck');
+const computerPaper = document.querySelector('.computerHand.ppr');
+const computerScissors = document.querySelector('.computerHand.scs');
+
+function displayPlayerRock(){
+    playerRock.style.display = "block";
+    playerPaper.style.display = "none";
+    playerScissors.style.display = "none";
+}
+function displayPlayerPaper(){
+    playerRock.style.display = "none";
+    playerPaper.style.display = "block";
+    playerScissors.style.display = "none";
+}
+function displayPlayerScissors(){
+    playerRock.style.display = "none";
+    playerPaper.style.display = "none";
+    playerScissors.style.display = "block";
+}
+function displayComputerRock(){
+    computerRock.style.display = "block";
+    computerPaper.style.display = "none";
+    computerScissors.style.display = "none";
+}
+function displayComputerPaper(){
+    computerRock.style.display = "none";
+    computerPaper.style.display = "block";
+    computerScissors.style.display = "none";
+}
+function displayComputerScissors(){
+    computerRock.style.display = "none";
+    computerPaper.style.display = "none";
+    computerScissors.style.display = "block";
+}
 
